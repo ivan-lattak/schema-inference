0b87fa0da1da297d355aa6a8a605c19e9aee6a5e
Ivan Latták <ivanlattak@gmail.com>
2021-05-18 16:45:57 +0200

    Extract InternalType natural ordering to its own class

--- impl/src/main/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/schema/InternalType.scala
+++ impl/src/main/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/schema/InternalType.scala
@@ -1,58 +1,72 @@
 package cz.cuni.mff.ksi.nosql.s13e.impl.inference.schema
 
 import cz.cuni.mff.ksi.nosql.s13e.impl.inference.schema.InternalEntityVersion.PropertiesOrdering
+import cz.cuni.mff.ksi.nosql.s13e.impl.inference.schema.InternalType.NaturalOrdering
 import cz.cuni.mff.ksi.nosql.s13e.impl.inference.util.OptionOrdering
 
 import scala.annotation.tailrec
 
 sealed abstract case class InternalType() extends Ordered[InternalType] {
 
-  override def compare(that: InternalType): Int = InternalType.compare(this, that)
+  override def compare(that: InternalType): Int = NaturalOrdering.compare(this, that)
 
 }
 
 case object InternalType {
 
-  def compare(x: InternalType, y: InternalType): Int = subtypeOrder(x) - subtypeOrder(y) match {
-    case c if c != 0 => c
-    case _ => (x, y) match {
-      case (InternalArray(left), InternalArray(right)) => left.compare(right)
-      case (InternalAggregate(left), InternalAggregate(right)) => PropertiesOrdering.compare(left, right)
-      case (InternalEntityReference(leftTarget, leftOrig), InternalEntityReference(rightTarget, rightOrig)) =>
-        leftTarget.compare(rightTarget) match {
-          case c if c != 0 => c
-          case _ => OptionOrdering.compareOptions(leftOrig, rightOrig)
-        }
-      case (InternalUnionType(left), InternalUnionType(right)) => compareLists(left, right)
-      case _ => 0 // other subtypes do not have parameters
-    }
-  }
+  private case object NaturalOrdering extends Ordering[InternalType] {
 
-  private def subtypeOrder(t: InternalType): Int = t match {
-    case InternalUnknownType => 0
-    case InternalBoolean => 1
-    case InternalNumber => 2
-    case InternalString => 3
-    case InternalArray(_) => 4
-    case InternalAggregate(_) => 5
-    case InternalEntityReference(_, _) => 6
-    case InternalUnionType(_) => 7
-    case _ => throw new RuntimeException("Not all cases covered")
-  }
-
-  @tailrec
-  private def compareLists(left: List[InternalSingleType], right: List[InternalSingleType]): Int = (left, right) match {
-    case (Nil, Nil) => 0
-    case (Nil, _ :: _) => -1
-    case (_ :: _, Nil) => 1
-    case (lh :: lt, rh :: rt) => lh.compare(rh) match {
+    override def compare(x: InternalType, y: InternalType): Int = SubtypeOrdering.compare(x, y) match {
       case c if c != 0 => c
-      case _ => compareLists(lt, rt)
+      case _ => (x, y) match {
+        case (InternalArray(left), InternalArray(right)) => left.compare(right)
+        case (InternalAggregate(left), InternalAggregate(right)) => PropertiesOrdering.compare(left, right)
+        case (InternalEntityReference(leftTarget, leftOrig), InternalEntityReference(rightTarget, rightOrig)) =>
+          leftTarget.compare(rightTarget) match {
+            case c if c != 0 => c
+            case _ => OptionOrdering.compareOptions(leftOrig, rightOrig)
+          }
+        case (InternalUnionType(left), InternalUnionType(right)) => compareLists(left, right)
+        case _ => 0 // other subtypes do not have parameters
+      }
     }
+
+    @tailrec
+    private def compareLists(left: List[InternalSingleType], right: List[InternalSingleType]): Int = (left, right) match {
+      case (Nil, Nil) => 0
+      case (Nil, _ :: _) => -1
+      case (_ :: _, Nil) => 1
+      case (lh :: lt, rh :: rt) => lh.compare(rh) match {
+        case c if c != 0 => c
+        case _ => compareLists(lt, rt)
+      }
+    }
+
+  }
+
+  private case object SubtypeOrdering extends Ordering[InternalType] {
+
+    override def compare(x: InternalType, y: InternalType): Int = order(x) - order(y)
+
+    private def order(t: InternalType): Int = t match {
+      case InternalUnknownType => 0
+      case InternalBoolean => 1
+      case InternalNumber => 2
+      case InternalString => 3
+      case InternalArray(_) => 4
+      case InternalAggregate(_) => 5
+      case InternalEntityReference(_, _) => 6
+      case InternalUnionType(_) => 7
+      case _ => throw new RuntimeException("Not all cases covered")
+    }
+
   }
 
 }
 
+/**
+ * @param types sorted
+ */
 sealed case class InternalUnionType(types: List[InternalSingleType]) extends InternalType
 
 sealed abstract case class InternalSingleType() extends InternalType
@@ -70,6 +84,9 @@ case object InternalString extends InternalPrimitiveType
 
 sealed abstract case class InternalComplexType() extends InternalSingleType
 
+/**
+ * @param target can change only to a version equivalent according to PropertiesOrdering
+ */
 sealed case class InternalAggregate(private[schema] var target: InternalEntityVersion) extends InternalComplexType
 
 case object InternalAggregate {
