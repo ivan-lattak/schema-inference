8226a7ce9a93131959331c89aca63adc2c35f625
Ivan Latták <ivanlattak@gmail.com>
2021-05-19 02:49:29 +0200

    Fix scalatest vs. JUnit 5 issues

--- impl/src/test/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/UnitTest.scala
+++ impl/src/test/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/UnitTest.scala
@@ -1,5 +1,6 @@
 package cz.cuni.mff.ksi.nosql.s13e.impl.inference
 
 import org.scalatest.funspec.PathAnyFunSpec
+import org.scalatest.matchers.should.Matchers
 
-class UnitTest extends PathAnyFunSpec
+class UnitTest extends PathAnyFunSpec with Matchers
--- impl/src/test/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/util/OptionOrderingTest.scala
+++ impl/src/test/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/util/OptionOrderingTest.scala
@@ -4,4 +4,10 @@ import cz.cuni.mff.ksi.nosql.s13e.impl.inference.UnitTest
 
 class OptionOrderingTest extends UnitTest {
 
+  describe("OptionOrdering") {
+    it("should correctly compare different options") {
+      OptionOrdering.compareOptions(None, Some(1)) should be < 0
+    }
+  }
+
 }
