f7fadf9e20336f5057595c3dcf2cd71b5982dc0b
Ivan Latták <ivanlattak@gmail.com>
2021-05-22 21:59:07 +0200

    Implement InternalPropertyTest

--- impl/src/test/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/schema/InternalPropertyTest.scala
+++ impl/src/test/scala/cz/cuni/mff/ksi/nosql/s13e/impl/inference/schema/InternalPropertyTest.scala
@@ -1,7 +1,38 @@
 package cz.cuni.mff.ksi.nosql.s13e.impl.inference.schema
 
 import cz.cuni.mff.ksi.nosql.s13e.impl.inference.UnitTest
+import cz.cuni.mff.ksi.nosql.s13e.impl.inference.util.Defaults
 
-class InternalPropertyTest extends UnitTest {
+class InternalPropertyTest extends UnitTest with Defaults {
+
+  describe("InternalProperty's natural ordering") {
+
+    val propertyAStrOpt = InternalProperty("a", string, optional = true)
+    val propertyCBool = InternalProperty("c", boolean)
+
+    it("should order by name first") {
+      val propertyBNumOpt = InternalProperty("b", number, optional = true)
+
+      propertyAStrOpt should be < propertyBNumOpt
+      propertyBNumOpt should be < propertyCBool
+    }
+
+    it("should order by optionality, when name is the same") {
+      val propertyAStr = propertyAStrOpt.copy(optional = false)
+      val propertyCBoolOpt = propertyCBool.copy(optional = true)
+
+      propertyAStr should be < propertyAStrOpt
+      propertyCBool should be < propertyCBoolOpt
+    }
+
+    it("should order by type, when name and optionality are the same") {
+      val propertyCNum = propertyCBool.copy(`type` = number)
+      val propertyCStr = propertyCBool.copy(`type` = string)
+
+      propertyCBool should be < propertyCNum
+      propertyCNum should be < propertyCStr
+    }
+
+  }
 
 }
